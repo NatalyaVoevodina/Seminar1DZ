6e21363f4844a7ac27339d0bbb062b05318c0fbe
natlya_voevodina <voevodochka1988@yandex.ru>
2023-02-14 12:20:42 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("это четное число");
+}
+else
+{
+    Console.WriteLine("это не четное число");
+}
